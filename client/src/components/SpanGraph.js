8938f7c1ab720e49007cc060a4a663290d2100f3
Untoldwind <landru@untoldwind.net>
2015-12-28 13:48:22 +0100

    Refactoring

--- client/src/components/SpanGraph.js
+++ client/src/components/SpanGraph.js
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactART from 'react-art'
 import ReactDOM from 'react-dom'
 import FetchingComponent from './FetchingComponent'
+import Rectangle from 'react-art/lib/Rectangle.art'
 
 import {throttle} from '../utils/dash'
 import {formatTotalString} from './formats'
+
+const {Surface, Group, Shape, Text, LinearGradient, Path} = ReactART
 
 export default class SpanGraph extends FetchingComponent {
     static propTypes = {
@@ -39,26 +42,32 @@ export default class SpanGraph extends FetchingComponent {
         const inGraph = this.createGraph(-1, "bytes_in", max, deltas)
         const outGraph = this.createGraph(1, "bytes_out", max, deltas)
 
-        const upperRect = ReactART.Path().moveTo(0, height/2).lineTo(width, height/2).lineTo(width, 0).lineTo(0, 0)
-        const lowerRect = ReactART.Path().moveTo(0, height/2).lineTo(width, height/2).lineTo(width, height).lineTo(0, height)
-        const maxInLine = ReactART.Path().moveTo(width * 0.1, height * 0.25).lineTo(width * 0.1 + (height * 0.25), 5).lineTo(width, 5)
-        const maxOutLine = ReactART.Path().moveTo(width * 0.1, height * 0.75).lineTo(width * 0.1 + (graphHeight - height * 0.75), graphHeight).lineTo(width, graphHeight)
-        const baseLine = ReactART.Path().moveTo(0, height/2).lineTo(width, height/2)
+        const upperRect = Path().moveTo(0, height/2).lineTo(width, height/2).lineTo(width, 0).lineTo(0, 0)
+        const lowerRect = Path().moveTo(0, height/2).lineTo(width, height/2).lineTo(width, height).lineTo(0, height)
+        const inLabelX = width * 0.1
+        const inLabelY =  height * 0.25
+        const maxInLine = Path().moveTo(inLabelX, inLabelY).lineTo(inLabelX + inLabelY - 5, 5).lineTo(width, 5)
+        const outLabelX = width * 0.1
+        const outLabelY = height * 0.75
+        const maxOutLine = Path().moveTo(outLabelX, outLabelY).lineTo(outLabelX + (graphHeight - outLabelY) - 5, graphHeight).lineTo(width, graphHeight)
+        const baseLine = Path().moveTo(0, height/2).lineTo(width, height/2)
 
         return (
-            <ReactART.Surface ref="node" width={width} height={height} style={{backgroundColor: "black"}}>
-                <ReactART.Group>
-                    <ReactART.Shape fill={new ReactART.LinearGradient(['#040', '#000'], 0, height/2, 0, 0)} d={upperRect}/>
-                    <ReactART.Shape fill={new ReactART.LinearGradient(['#400', '#000'], 0, height/2, 0, height)} d={lowerRect}/>
-                    <ReactART.Shape fill="#0f0" d={inGraph}/>
-                    <ReactART.Shape fill="#f00" d={outGraph}/>
-                    <ReactART.Shape stroke="#aaa" d={maxInLine}/>
-                    <ReactART.Shape stroke="#aaa" d={maxOutLine}/>
-                    <ReactART.Text fill="#ddd" font='16px "Arial"' alignment='middle' x={width * 0.1} y={height * 0.25}>{formatTotalString(max.bytes_in)}</ReactART.Text>
-                    <ReactART.Text fill="#ddd" font='16px "Arial"' alignment='middle' x={width * 0.1} y={height * 0.75 - 16}>{formatTotalString(max.bytes_out)}</ReactART.Text>
-                    <ReactART.Shape stroke="#fff" d={baseLine}/>
-                </ReactART.Group>
-            </ReactART.Surface>
+            <Surface ref="node" width={width} height={height} style={{backgroundColor: "black"}}>
+                <Group>
+                    <Shape fill={new LinearGradient(['#040', '#000'], 0, height/2, 0, 0)} d={upperRect}/>
+                    <Shape fill={new LinearGradient(['#400', '#000'], 0, height/2, 0, height)} d={lowerRect}/>
+                    <Shape fill="#0f0" d={inGraph}/>
+                    <Shape fill="#f00" d={outGraph}/>
+                    <Shape stroke="#aaa" d={maxInLine}/>
+                    <Shape stroke="#aaa" d={maxOutLine}/>
+                    <Rectangle fill="#222" x={inLabelX - 50} y={inLabelY - 10} width={100} height={20} radius={5}/>
+                    <Rectangle fill="#222" x={outLabelX - 50} y={outLabelY - 10} width={100} height={20} radius={5}/>
+                    <Text fill="#eee" font='16px "Arial"' alignment='middle' x={inLabelX} y={inLabelY - 8}>{formatTotalString(max.bytes_in)}</Text>
+                    <Text fill="#eee" font='16px "Arial"' alignment='middle' x={outLabelX} y={outLabelY - 8}>{formatTotalString(max.bytes_out)}</Text>
+                    <Shape stroke="#fff" d={baseLine}/>
+                </Group>
+            </Surface>
         )
     }
 
